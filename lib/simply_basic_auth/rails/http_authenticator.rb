6929f31931a71e58fd0b4631ce73e2433e04f249
Brett Wilkins <brett@brett.geek.nz>
2014-07-10 17:21:20 +1000

    It's *Action* Controller, not active controller. Also, write the authenticator in a non-Concern manner

--- lib/simply_basic_auth/rails/http_authenticator.rb
+++ lib/simply_basic_auth/rails/http_authenticator.rb
@@ -1,21 +1,26 @@
 require 'simply_basic_auth/yaml_reader'
 require 'simply_basic_auth/user_validator'
-require 'active_controller/metal/http_authentication'
+require 'action_controller/metal/http_authentication'
 
 module SimplyBasicAuth
   module Rails
     module HttpAuthenticator
-      include SimplyBasicAuth::YamlReader
-      include SimplyBasicAuth::UserValidator
-      include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-      included do
-        before_filter :authenticate!
+      def self.included(base)
+
+        base.send(:include, InstanceMethods)
+        base.send(:before_filter, :authenticate!)
       end
 
-      def authenticate!
-        authenticate_or_request_with_http_basic do |username, password|
-          valid_user?(username, password, Rails.env)
+      module InstanceMethods
+        include SimplyBasicAuth::YamlReader
+        include SimplyBasicAuth::UserValidator
+        include ActionController::HttpAuthentication::Basic::ControllerMethods
+
+        def authenticate!
+          authenticate_or_request_with_http_basic do |username, password|
+            valid_user?(username, password, ::Rails.env)
+          end
         end
       end
 
